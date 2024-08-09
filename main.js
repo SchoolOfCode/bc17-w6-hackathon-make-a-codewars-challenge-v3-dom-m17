5875664437bae37e52edc84fa5c142f94a4dc5b0
dom-m17 <dominicmaynard00@gmail.com>
2024-08-09 10:50:49 +0100

    removing solution

--- main.js
+++ main.js
@@ -1,43 +1,34 @@
 /*
 
-*Count of positives / sum of negatives*
+*Sum of positives / sum of negatives*
 
 Given an array of integers.
 
-Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+Return an array, where the first element is the sum of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+Example:
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [55, -65].
+
 
 If the input is an empty array or is null, return an empty array.
 
-Example
-For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
-If no positive/negative numbers
+Example:
+For input [], you should return []
+
+
+If the input contains only positive/negative numbers but not both, the function should return 0 in place of the respective sum.
+
+Example:
+For input [-1, -2, -3], you should return [0, -6]
+
+
+For any invalid input (anything that is not an array of numbers), the function should throw an error.
+
+Example:
+For input "School of Code", {"Paris": "Olympics"} or [1, [2, 3]], you should throw an error.
+
 */
 
-export default function calculatePostiveAndNegative(input) {
-    
-    if (!Array.isArray(input)) {
-        throw new Error
-    }
+export default function calculatePostiveAndNegative(nums) {
 
-    for (let i = 0; i < input.length; i++) {
-        if (typeof input[i] !== "number") {
-            throw new Error
-        }
-    }
-
-    if (input == null || input.length == 0)
-        return [];
-      
-      var positive = 0;
-      var negative = 0;
-      
-      for (var i=0, l=input.length; i<l; ++i)
-      {
-        if (input[i] > 0)
-          positive += input[i];
-        else
-          negative += input[i];
-      }
-      
-      return [positive, negative];
 }
